65fe999bf6d90c48d8dff7d5853cfd7fc7191696
Júlia Stella <juliastellapaula@gmail.com>
2021-03-26 22:38:59 -0300

    criação de variável e função no TS. Modificação das cores de texto no scss

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.page.scss'],
 })
 export class FeedPage implements OnInit {
-
+  public nome_usuario: string = "Coragem, O Cão Covarde, teste Ts";
   constructor() { }
 
+ // public somaDoisNumeros(num1:number, num2:number): void {
+
+    // alert(num1 + num2);
+
+  //}
+
   ngOnInit() {
+    //this.somaDoisNumeros(10, 99);
   }
 
 }
